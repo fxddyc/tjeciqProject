d1c7f4432b424de4f7a2054448a8746e6ed619c8
simon <wangcmic@hotmail.com>
2016-08-23 15:31:40 +0800

    None

--- tjpcms/src/main/java/cn/com/eship/TestMain.java
+++ tjpcms/src/main/java/cn/com/eship/TestMain.java
@@ -2,6 +2,8 @@ package cn.com.eship;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 
@@ -12,13 +14,18 @@ import org.apache.hadoop.hbase.client.HBaseAdmin;
 public class TestMain {
 	public static void main(String[] args) throws Exception {
 		Configuration config = HBaseConfiguration.create();
-	       config.set("hbase.zookeeper.quorum",  
-                    "sandbox.hortonworks.com");  
+	       config.set("hbase.zookeeper.quorum",
+                    "sandbox.hortonworks.com");
+	       config.set("zookeeper.znode.parent", "/hbase-unsecure");
         config.set("hbase.zookeeper.property.clientPort", "2181");  
         config.set("hbase.cluster.distributed", "true");
+        HTableDescriptor hTableDescriptor = new HTableDescriptor("epidemicInfo");
+        HColumnDescriptor hColumnDescriptor = new HColumnDescriptor("c1");
+        hTableDescriptor.addFamily(hColumnDescriptor);
         HBaseAdmin hAdmin = new HBaseAdmin(config);
-        TableName[] tableNames = hAdmin.listTableNames();
-        System.out.println(tableNames);
+        hAdmin.createTable(hTableDescriptor);
+        
+        
         
 	}
 }

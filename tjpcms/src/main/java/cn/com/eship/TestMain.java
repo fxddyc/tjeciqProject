2996a516128a0cf0955c13ce722cc591b3b5a3fa
simon <wangcmic@hotmail.com>
2016-08-23 12:26:27 +0800

    解决Hbase在工程依赖冲突问题

--- tjpcms/src/main/java/cn/com/eship/TestMain.java
+++ tjpcms/src/main/java/cn/com/eship/TestMain.java
@@ -1,37 +1,24 @@
 package cn.com.eship;
 
-import cn.com.eship.dao.EpidemicAppearDao;
-import cn.com.eship.dao.impl.EpidemicAppearDaoImpl;
-import cn.com.eship.model.EpidemicAppear;
-import cn.com.eship.service.EpidemicService;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
 
 /**
  * Created by simon on 16/7/14.
  */
 public class TestMain {
-    public static void main(String[] args) throws Exception {
-        getDirectoryFromHdfs();
-    }
-
-    private static void getDirectoryFromHdfs() throws FileNotFoundException, IOException {
-        String dst = "hdfs://sandbox.hortonworks.com:8020";
-        Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(URI.create(dst), conf);
-        FileStatus fileList[] = fs.listStatus(new Path(dst));
-        int size = fileList.length;
-        for (int i = 0; i < size; i++) {
-            System.out.println("name:" + fileList[i].getPath().getName() + "/t/tsize:" + fileList[i].getLen());
-        }
-        fs.close();
-    }
+	public static void main(String[] args) throws Exception {
+		Configuration config = HBaseConfiguration.create();
+	       config.set("hbase.zookeeper.quorum",  
+                    "sandbox.hortonworks.com");  
+        config.set("hbase.zookeeper.property.clientPort", "2181");  
+        config.set("hbase.cluster.distributed", "true");
+        HBaseAdmin hAdmin = new HBaseAdmin(config);
+        TableName[] tableNames = hAdmin.listTableNames();
+        System.out.println(tableNames);
+        
+	}
 }

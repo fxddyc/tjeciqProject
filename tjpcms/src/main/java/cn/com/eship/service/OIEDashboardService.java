9e9d97ce35baace1f2418ed65d08289526fb746c
guoji <guo_jibs@outlook.com>
2017-09-19 16:32:52 +0800

    jsp update

---
+++ tjpcms/src/main/java/cn/com/eship/service/OIEDashboardService.java
@@ -0,0 +1,7 @@
+package cn.com.eship.service;
+
+public interface OIEDashboardService {
+    String getDiseaseClassPieData(int dateInterval) throws Exception;
+    String getCalendarHeatMapData() throws Exception;
+    String getDiseaseEventListData(String pageNo,String startDate,String endDate,String epidemicClass)throws Exception;
+}
